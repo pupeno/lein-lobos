5544263923a78d3876c4c848beb5368eaef290cf
J. Pablo Fernández <pupeno@pupeno.com>
2011-08-20 06:36:39 +0200

    Start at version 0.1.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 ;;;; Copyright © 2011 José Pablo Fernández Silva
-(defproject lein-lobos "1.0.0-SNAPSHOT"
+(defproject lein-lobos "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]])
