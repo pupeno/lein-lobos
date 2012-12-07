6454c880247e73e1f6624e12f12e643587389aa4
Harry <harry.robertson@gmail.com>
2012-12-07 08:24:19 -0800

    Bumping version to match lobos and reflect fixes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 ;;;; Copyright © 2011 José Pablo Fernández Silva
-(defproject lein-lobos "1.0.0-SNAPSHOT"
+(defproject lein-lobos "1.0.0-beta1"
   :description "Leiningen plugin for running Lobos commands."
   :eval-in-leiningen true)
